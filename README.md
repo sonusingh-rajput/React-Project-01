13e2e5c4951811a2a1293e93f08e817cfd93fb77
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-16 19:24:17 +0530

    React project Nav complete

---
+++ README.md
@@ -0,0 +1,4 @@
+React Project 1
+
+Description: This Project is create mini sote for useing API.
+API:https://fakestoreapi.com/product
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen">
+      <nav className="w-[15%] bg-zinc-100 h-full flex flex-col items-center">
+        <a className="px-3 py-1 mt-5 border border-blue-300 text-blue-300 rounded">
+          Add New Product
+        </a>
+        <hr className="bg-zinc-500 my-3 w-[80%]" />
+        <h2 className="text-2xl mb-3 w-[80%] font-semibold">Category Filter</h2>
+        <ul className="w-[80%] flex flex-col gap-3">
+          <li className="flex gap-2 items-center">
+            <span className="rounded-full w-[18px] h-[18px] bg-green-100 " />
+            Cart 1
+          </li>
+          <li className="flex gap-2 items-center">
+            <span className="rounded-full w-[18px] h-[18px] bg-blue-100 " />
+            Cart 1
+          </li>
+          <li className="flex gap-2 items-center">
+            <span className="rounded-full w-[18px] h-[18px] bg-pink-100 " />
+            Cart 1
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
