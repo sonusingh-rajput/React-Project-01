71dec1ab916943b12cbfcd4f21d13f43533901ca
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-17 07:49:05 +0530

    Reinitilize the tailwind for not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div className="h-screen w-screen">
-      <nav className="w-[15%] bg-zinc-100 h-full flex flex-col items-center">
+    <div className="h-screen w-screen flex">
+      <nav className="w-[20%] bg-zinc-100 h-full flex flex-col items-center">
         <a className="px-3 py-1 mt-5 border border-blue-300 text-blue-300 rounded">
           Add New Product
         </a>
@@ -24,6 +22,20 @@ const App = () => {
           </li>
         </ul>
       </nav>
+
+      <div className="w-full flex flex-wrap p-10 overflow-x-hidden overflow-y-auto  ">
+        <div className="border-sky-200 border mr-3 mb-3 w-[13vw] h-[25vh] shadow hover:scale-105 transition-all cursor-pointer">
+          <div
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+            }}
+            className="w-full h-[80%] bg-contain bg-no-repeat bg-center p-2"
+          ></div>
+          <p className="mt-2 font-semibold text-center hover:text-blue-400">Lorem id to sand pic</p>
+        </div>
+        
+      </div>
     </div>
   );
 };
